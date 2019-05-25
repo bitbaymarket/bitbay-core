185a6d9d76005b62af32547f7578ebacb0ed404f
yshurik <yshurik@gmail.com>
2019-05-25 21:14:10 +0200

    Fix typo in script

--- contrib/docker/baystart.sh
+++ contrib/docker/baystart.sh
@@ -6,8 +6,7 @@ touch /data/bitbay.conf
 echo "rpcport=80" > /data/bitbay.conf
 echo "rpcuser=${RPC_USER}" >> /data/bitbay.conf
 echo "rpcpassword=${RPC_PASS}" >> /data/bitbay.conf
-echo "rpcallowip=*
-echo args: $*" >> /data/bitbay.conf
+echo "rpcallowip=*" >> /data/bitbay.conf
 cat /data/bitbay.conf
 
 bitbayd -datadir=/data
